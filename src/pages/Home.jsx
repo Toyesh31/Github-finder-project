c6089c3a6dc24caa73492d4c2bd0f84cf4c38047
Toyesh31 <tdashjitu@gmail.com>
2022-03-17 00:40:13 +0530

    created github context, github reducer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import UserResults from "../components/users/UserResults";
 function Home() {
   return (
     <div>
-      <h1 className="text-6xl">Welcome</h1>
       <UserResults />
     </div>
   );
